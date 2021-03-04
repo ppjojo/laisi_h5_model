b44cacdca254f751aa38688e5edd112bf336889d
junyu.gao <junyu.gao@laisitech.com>
2021-03-04 10:51:14 +0800

    Signed-off-by: junyu.gao <junyu.gao@laisitech.com>

---
+++ myGroup/src/api/groupMember.js
@@ -0,0 +1,17 @@
+import request from "@u/request"
+export function listItem(data) {
+    return request({
+        url: 'club/clubGroupInfo',
+        method: 'get',
+        data,
+        params:data
+    })
+}
+export function TakeFocus(data) {
+    return request({
+        url: 'community-core/interest/add',
+        method: 'post',
+        data,
+        params:data
+    })
+}
