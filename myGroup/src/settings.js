697143074b18934f22668453cfd63251bd5b3f39
handsome <hhhh1006@163.com>
2021-03-05 13:20:58 +0800

    setting修改

--- myGroup/src/settings.js
+++ myGroup/src/settings.js
@@ -1,3 +1,7 @@
+const path = require("path");
+function resolve(dir) {
+	return path.join(__dirname, dir);
+  }
 module.exports = {
 	title: 'LAISITECH',
 	host: process.env.NODE_ENV != 'production' ? 'https://lstest.laisitech.com/' : 'https://lsprod.laisitech.com/',
@@ -5,36 +9,36 @@ module.exports = {
 			id: 1,
 			name: "跳绳",
 			class: "ts",
-			img: require('./img/label_ts.svg'),
-			activeImg: require('./img/label_ts_color.svg'),
+			img:resolve('src/img/label_ts.svg'),
+			activeImg:resolve('src/img/label_ts_color.svg'),
 		},
 		{
 			id: 2,
 			name: "减肥",
 			class: "jf",
-			img: require('./img/label_jf.svg'),
-			activeImg: require('./img/label_jf_color.svg'),
+			img: resolve('src/img/label_jf.svg'),
+			activeImg: resolve('src/img/label_jf_color.svg'),
 		},
 		{
 			id: 3,
 			name: "减脂",
 			class: "jz",
-			img: require('./img/label_jz.svg'),
-			activeImg: require('./img/label_jz_color.svg'),
+			img: resolve('src/img/label_jz.svg'),
+			activeImg: resolve('src/img/label_jz_color.svg'),
 		},
 		{
 			id: 4,
 			name: "健身",
 			class: "js",
-			img: require('./img/label_js.svg'),
-			activeImg: require('./img/label_js_color.svg'),
+			img: resolve('src/img/label_js.svg'),
+			activeImg: resolve('src/img/label_js_color.svg'),
 		},
 		{
 			id: 5,
 			name: "腕力球",
 			class: "wlq",
-			img: require('./img/label_wlq.svg'),
-			activeImg: require('./img/label_wlq_color.svg'),
+			img: resolve('src/img/label_wlq.svg'),
+			activeImg: resolve('src/img/label_wlq_color.svg'),
 		},
 	],
 	RETURN_LABEL(id){
