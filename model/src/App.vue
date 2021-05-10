07497ccff3a41147f2265da07eae9c8979e34229
handsome <hhhh1006@163.com>
2021-05-10 14:13:02 +0800

    跳转动画的修改

--- model/src/App.vue
+++ model/src/App.vue
@@ -20,11 +20,14 @@
             $route(to, from) {
                 //如果to的索引值为0，不添加任何动画；如果to索引大于from索引,判断为前进状态,反之则为后退状态
                  if(to.meta.index > 0){
-                    if( to.meta.index < from.meta.index){
+                   if(from.meta.index){
+                     if( to.meta.index < from.meta.index){
                         this.transitionName = 'slide-right';
                     }else{
                         this.transitionName = 'slide-left';
                     }
+                   }
+                    
                 }else if(to.meta.index == 0 && from.meta.index > 0){
                     this.transitionName = 'slide-right';
                 }
