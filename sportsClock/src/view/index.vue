be0d13e487e9b3e297463d96639b65f748502bab
junyu.gao <junyu.gao@laisitech.com>
2021-07-07 09:47:26 +0800

    Signed-off-by: junyu.gao <junyu.gao@laisitech.com>

---
+++ sportsClock/src/view/index.vue
@@ -0,0 +1,116 @@
+<template>
+	<div id="app" v-cloak>
+		<div class="header">
+			<van-nav-bar title="运动日历" right-text="打卡总览" @click-left="onClickLeft" @click-right="onClickRight" left-arrow
+				safe-area-inset-top fixed>
+			</van-nav-bar>
+		</div>
+		<nCalendar ref="calendar"></nCalendar>
+		<!-- 时间选择 -->
+		<van-popup position="bottom" round :style="{height: '45%'}" v-model="YMshow">
+			<van-datetime-picker v-model="currentDate" type="year-month" title="选择年月" :min-date="minDate"
+			@confirm="pickConfirm"
+				:max-date="maxDate" :formatter="formatter">
+			</van-datetime-picker>
+		</van-popup>
+		<!-- 打卡状态 -->
+		<van-popup  round :style="{height: '45%'}" v-model="clockShow">
+			<clockState></clockState>
+		</van-popup>
+	</div>
+</template>
+
+<script>
+	import nCalendar from '@c/calendar';
+	import clockState from '@c/clockState';
+	import {
+		listItem
+	} from '@a/test'
+	import {
+		NavBar,
+		Icon,
+		Popup,
+		DatetimePicker
+	} from 'vant';
+
+	export default {
+		components: {
+			[NavBar.name]: NavBar,
+			[Icon.name]: Icon,
+			nCalendar,clockState,
+			[Popup.name]: Popup,
+			[DatetimePicker.name]: DatetimePicker,
+			// [Swipe.name]: Swipe,
+			// [SwipeItem.name]: SwipeItem,
+			// [GoodsAction.name]: GoodsAction,
+			// [GoodsActionIcon.name]: GoodsActionIcon,
+			// [GoodsActionButton.name]: GoodsActionButton
+		},
+
+		data() {
+			return {
+				minDate: new Date(2021, 0, 1),
+				maxDate: new Date(),
+				currentDate: new Date(),
+				YMshow: false,
+				clockShow:true,
+			};
+		},
+		filters: {},
+		mounted() {
+			this.getList()
+		},
+		created() {},
+		methods: {
+			getList() {
+				listItem().then(() => {
+					console.log("success")
+				}).catch(() => {
+					console.log("error")
+				})
+			},
+			onClickLeft() {
+				this.$interaction.closePage();
+				// this.$router.go(-1)
+			},
+			onClickRight() { //跳转创建小组
+				this.$router.push({
+					path: '/groupIndex',
+					query: {
+						id: item
+					}
+				});
+			},
+			goGroupIndex(item) {
+				this.$router.push({
+					path: '/groupIndex',
+					query: {
+						id: item
+					}
+				});
+
+			},
+			fatherPickYearMonth() { //子组件调用打开时间选择
+				this.YMshow = true;
+			},
+			pickConfirm(value){//时间选择回调
+				this.$refs.calendar.getList(value)
+				this.$refs.calendar.dateTitleStr(value,'ym')
+				this.YMshow = false;
+			},
+			formatter(type, val) {
+				if (type === 'year') {
+					return `${val}年`;
+				} else if (type === 'month') {
+					return `${val}月`;
+				}
+				return val;
+			},
+		}
+	};
+</script>
+<style lang="scss">
+	.van-picker__mask{
+		display: none;
+	}
+</style>
---
+++ sportsClock/vue.config.js
@@ -0,0 +1,166 @@
+"use strict";
+const path = require("path");
+const autoprefixer = require("autoprefixer");
+const pxtorem = require("postcss-pxtorem");
+const vantConfig = path.join(__dirname, "./src/styles/vantConfig.less");
+const defaultSettings = require("./src/settings.js");
+const Compression = require('compression-webpack-plugin')
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
+const name = defaultSettings.title || "LAISITECH"; // page title
+
+// If your port is set to 80,
+// use administrator privileges to execute the command line.
+// For example, Mac: sudo npm run
+// You can change the port by the following methods:
+// port = 9528 npm run dev OR npm run dev --port = 9528
+const port = process.env.port || process.env.npm_config_port || 8080; // dev port
+
+// All configuration item explanations can be find in https://cli.vuejs.org/config/
+module.exports = {
+  /**
+   * You will need to set publicPath if you plan to deploy your site under a sub path,
+   * for example GitHub Pages. If you plan to deploy your site to https://foo.github.io/bar/,
+   * then publicPath should be set to "/bar/".
+   * In most cases please use '/' !!!
+   * Detail: https://cli.vuejs.org/config/#publicpath
+   */
+  publicPath: process.env.NODE_ENV === "production" ?
+    "/h5/h5V2/weekPlan" : "/h5/h5V2/weekPlan",
+  outputDir: "dist",
+  assetsDir: "static",
+  lintOnSave: process.env.NODE_ENV === "development",
+  productionSourceMap: false,
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          autoprefixer(),
+          pxtorem({
+            rootValue: 50,
+            propList: ["*"],
+          }),
+        ],
+      },
+      less: {
+        modifyVars: {
+          hack: `true; @import "${vantConfig}";`,
+        },
+        // 若 less-loader 版本小于 6.0，请移除 lessOptions 这一级，直接配置选项。
+        // lessOptions: {
+        //   modifyVars: {
+        //     // 直接覆盖变量
+        //     red: '#1989fa',
+        //     blue: '#ee0a24',
+        //     orange: '#f08d49',
+        //     white:"#000",
+        //     'text-color': '#111',
+        //     'active-color': '#B4E7A0',
+        //     // 或者可以通过 less 文件覆盖（文件路径为绝对路径）
+        //      hack: `true; @import "${vantConfig}";`,
+        //   },
+        // },
+      },
+    },
+  },
+  devServer: {
+    port: port,
+    open: true,
+    overlay: {
+      warnings: false,
+      errors: true,
+    },
+    // proxy: {
+    //   '/rope-skipping-summer-camp': {
+    //       target: 'https://lstest.laisitech.com',
+    //       //ws: true,    // 启用websockets
+    //       secure: false,
+    //       changeOrigin: true,
+    //     //   pathRewrite:{
+    //     //     '/rope-skipping-summer-camp':'/rope-skipping-summer-camp11111'
+    //     // }
+    //   }
+    // },
+    //before: require('./mock/mock-server.js')
+  },
+  configureWebpack: config => {
+    let baseConfig={
+      name: name,
+        resolve: {
+          alias: {
+            "@": resolve("src"),
+            "@u": resolve("src/utils"),
+            "@a": resolve("src/api"),
+            "@s": resolve("src/style"),
+			"@c": resolve("src/components"),
+          },
+        },
+    }
+
+    if (process.env.NODE_ENV != "dev") {
+      baseConfig.plugins=[
+        new Compression({
+          test: /\.js$|\.html$|\.css$/, // 选择压缩的 文件格式
+          threshold: 10240, //超过10k启动gzip压缩
+          deleteOriginalAssets: false //删除源文件
+        })
+      ]
+      return baseConfig
+    }else{
+      return baseConfig
+    }
+
+  },
+  chainWebpack(config) {
+    config.plugins.delete("preload"); // TODO: need test
+    config.plugins.delete("prefetch"); // TODO: need test
+
+    
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .loader("vue-loader")
+      .tap((options) => {
+        options.compilerOptions.preserveWhitespace = true;
+        return options;
+      })
+      .end();
+
+    config.when(process.env.NODE_ENV === "development", (config) =>
+      config.devtool("cheap-source-map")
+    );
+
+    config.when(process.env.NODE_ENV !== "development", (config) => {
+      config
+        .plugin("ScriptExtHtmlWebpackPlugin")
+        .after("html")
+        .use("script-ext-html-webpack-plugin", [{
+          // `runtime` must same as runtimeChunk name. default is `runtime`
+          inline: /runtime\..*\.js$/,
+        }, ])
+        .end();
+      config.optimization.splitChunks({
+        chunks: "all",
+        cacheGroups: {
+          libs: {
+            name: "chunk-libs",
+            test: /[\\/]node_modules[\\/]/,
+            priority: 10,
+            chunks: "initial", // only package third parties that are initially dependent
+          },
+          commons: {
+            name: "chunk-commons",
+            test: resolve("src/components"), // can customize your rules
+            minChunks: 3, //  minimum common number
+            priority: 5,
+            reuseExistingChunk: true,
+          },
+        },
+      });
+      config.optimization.runtimeChunk("single");
+    });
+  },
+};
