0f717395b375b9e0fbdb16954d0ce7b6d1842658
handsome <hhhh1006@163.com>
2021-07-15 10:48:41 +0800

    运动周报

--- myGroup/src/api/myGroupList.js
+++
@@ -1,8 +0,0 @@
-import request from "@u/request"
-export function listItem(data) {
-    return request({
-        url: 'myteam/group/member/attend',
-        method: 'get',
-        params:data
-    })
-}
