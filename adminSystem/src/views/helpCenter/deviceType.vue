9b4f4f219b5d3f55d644e9f08a223afdc1bc80d8
haohao <hhhh1006@163.com>
2021-10-26 09:54:45 +0800

    帮助中心

--- adminSystem/src/views/helpCenter/deviceType.vue
+++ adminSystem/src/views/helpCenter/deviceType.vue
@@ -3,8 +3,7 @@
     <div class="container-edit">
       <el-button type="primary" size="mini" @click="btn_add()">添加设备类型</el-button>
     </div>
-    <el-table v-loading="loading" :data="list" element-loading-text="Loading" border fit highlight-current-row
-      size="small ">
+    <el-table v-loading="loading" :data="list" element-loading-text="Loading" border fit highlight-current-row size="small ">
       <el-table-column align="center" prop="deviceTypeDetail" label="设备类型名称">
       </el-table-column>
       <el-table-column align="center" prop="deviceType" label="设备类型">
@@ -59,176 +58,178 @@ wheel"></el-input>
 </template>
 
 <script>
-  import {
-    listItem,
-    addItem,
-    updateItem,
-    deleteItem
-  } from '@/api/helpCenter/devicetype'
-  import {
-    checkPermission
-  } from '@/api/checkPermission'
-  import {
-    formatDate
-  } from '@/utils/date'
-  import {
-    fileUpload
-  } from '@/utils/fileUpload'
-  export default {
-    filters: {
-      formatDate(time) {
-        time = time
-        let date = new Date(time)
-        return formatDate(date, 'yyyy-MM-dd hh:mm')
-      }
-
+import {
+  listItem,
+  addItem,
+  updateItem,
+  deleteItem,
+} from "@/api/helpCenter/devicetype";
+import { checkPermission } from "@/api/checkPermission";
+import { formatDate } from "@/utils/date";
+import { fileUpload } from "@/utils/fileUpload";
+export default {
+  filters: {
+    formatDate(time) {
+      time = time;
+      let date = new Date(time);
+      return formatDate(date, "yyyy-MM-dd hh:mm");
     },
-    computed: {
-      checkPer() {
-        return function(btn) {
-          return (checkPermission(this.$route.path + '/' + btn))
-        }
-      }
+  },
+  computed: {
+    checkPer() {
+      return function (btn) {
+        return checkPermission(this.$route.path + "/" + btn);
+      };
     },
-    data() {
-      return {
-        list: [],
-        loading: false,
-        dialogVisible: false,
-        dialogTitle: "",
-        form: {},
-        rules: {
-          picUrl: [{
+  },
+  data() {
+    return {
+      list: [],
+      loading: false,
+      dialogVisible: false,
+      dialogTitle: "",
+      form: {},
+      rules: {
+        picUrl: [
+          {
             required: true,
-            message: '请上传图片',
-            trigger: 'blur,change'
-          }, ],
-          deviceTypeDetail: [{
+            message: "请上传图片",
+            trigger: "blur,change",
+          },
+        ],
+        deviceTypeDetail: [
+          {
             required: true,
-            message: '请输入设备名称',
-            trigger: 'blur,change'
-          }, ],
-          deviceType: [{
+            message: "请输入设备名称",
+            trigger: "blur,change",
+          },
+        ],
+        deviceType: [
+          {
             required: true,
-            message: '请输入设备名称',
-            trigger: 'blur,change'
-          }, ],
-        },
-      }
+            message: "请输入设备名称",
+            trigger: "blur,change",
+          },
+        ],
+      },
+    };
+  },
+  mounted() {
+    this.getList();
+  },
+  methods: {
+    getList() {
+      listItem({}).then((res) => {
+        this.list = res.data;
+        this.loading = false;
+      });
     },
-    mounted() {
-      this.getList()
-    },
-    methods: {
-      getList() {
-        listItem({}).then(res => {
-          this.list = res.data;
-          this.loading = false
-        })
-      },
-      //保存
-      submitForm(formName) {
-        this.$refs[formName].validate((valid) => {
-          if (valid) {
-            this.dialogVisible = false
-            if (this.dialogTitle == "新增") {
-              addItem(this.form).then(response => {
-                this.getList()
-                this.$notify({
-                  type: 'success',
-                  message: '成功新增'
-                });
-              })
-            } else {
-              updateItem(this.form).then(response => {
-                this.getList()
-                this.$notify({
-                  type: 'success',
-                  message: '成功修改'
-                });
-              })
-            }
-          } else {
-            console.log('error submit!!');
-            return false;
-          }
-        });
-      },
-      btn_add() {
-        this.dialogVisible = true;
-        this.dialogTitle = "新增";
-        this.form = {
-          deviceTypeDetail: "",
-          picUrl: null,
-          deviceType:'',
-          orderNumber: 0,
-        }
-      },
-      btn_edit(row) {
-        this.dialogVisible = true;
-        this.dialogTitle = "编辑"
-        this.form = Object.assign({}, row)
-      },
-      //删除
-      btn_delete(id) {
-          this.$confirm('此操作将永久删除该设备, 是否继续?', '提示', {
-              confirmButtonText: '确定',
-              cancelButtonText: '取消',
-              type: 'warning'
-          }).then(() => {
-              deleteItem({
-                  id: id,
-              }).then(response => {
-                  this.getList()
-                  this.$notify({
-                      type: 'success',
-                      message: '成功删除所选设备!'
-                  });
-              })
-          }).catch(() => {
+    //保存
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.dialogVisible = false;
+          if (this.dialogTitle == "新增") {
+            addItem(this.form).then((response) => {
+              this.getList();
               this.$notify({
-                  message: '已取消删除操作设备',
-                  type: 'info'
+                type: "success",
+                message: "成功新增",
               });
-          });
-      },
-      requestFile(param) { //
-        var fileForm = new FormData()
-        fileForm.append('file', param.file)
-        fileUpload(fileForm).then(res => {
-          if (res.code == 0) {
-            this.form.picUrl = res.data.url;
+            });
+          } else {
+            updateItem(this.form).then((response) => {
+              this.getList();
+              this.$notify({
+                type: "success",
+                message: "成功修改",
+              });
+            });
           }
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    btn_add() {
+      this.dialogVisible = true;
+      this.dialogTitle = "新增";
+      this.form = {
+        deviceTypeDetail: "",
+        picUrl: null,
+        deviceType: "",
+        orderNumber: 0,
+      };
+    },
+    btn_edit(row) {
+      this.dialogVisible = true;
+      this.dialogTitle = "编辑";
+      this.form = Object.assign({}, row);
+    },
+    //删除
+    btn_delete(id) {
+      this.$confirm("此操作将永久删除该设备, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+        .then(() => {
+          deleteItem({
+            id: id,
+          }).then((response) => {
+            this.getList();
+            this.$notify({
+              type: "success",
+              message: "成功删除所选设备!",
+            });
+          });
         })
-      },
-    }
-  }
+        .catch(() => {
+          this.$notify({
+            message: "已取消删除操作设备",
+            type: "info",
+          });
+        });
+    },
+    requestFile(param) {
+      //
+      var fileForm = new FormData();
+      fileForm.append("file", param.file);
+      fileUpload(fileForm).then((res) => {
+        if (res.code == 0) {
+          this.form.picUrl = res.data.url;
+        }
+      });
+    },
+  },
+};
 </script>
 <style type="text/css">
-  .avatar-uploader .el-upload {
-    border: 1px dashed #d9d9d9;
-    border-radius: 6px;
-    cursor: pointer;
-    position: relative;
-    overflow: hidden;
-  }
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
 
-  .avatar-uploader .el-upload:hover {
-    border-color: #409EFF;
-  }
+.avatar-uploader .el-upload:hover {
+  border-color: #409eff;
+}
 
-  .avatar-uploader-icon {
-    font-size: 28px;
-    color: #8c939d;
-    width: 80px;
-    height: 80px;
-    line-height: 80px;
-    text-align: center;
-  }
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 80px;
+  height: 80px;
+  line-height: 80px;
+  text-align: center;
+}
 
-  .avatar {
-    width: 80px;
-    height: 80px;
-    display: block;
-  }
+.avatar {
+  width: 80px;
+  height: 80px;
+  display: block;
+}
 </style>
